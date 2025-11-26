ebeb82f0eea87560631b49f367f4d607aa3f002b
MEMEME <134994706+mijeong09@users.noreply.github.com>
2025-11-26 11:26:40 +0900

    Title: 변수 출력하기 3, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251126/변수 출력하기 3/outputing-variables-3.cpp
+++ 251126/변수 출력하기 3/outputing-variables-3.cpp
@@ -3,5 +3,10 @@ using namespace std;
 
 int main() {
     // Please write your code here.
+    int a = 1;
+    int b = 2;
+    char c = 'C';
+
+    cout << a << "->" << b << "->" << c;
     return 0;
 }
