011c0ff13c39f067951c20111792a57a06968960
MEMEME <134994706+mijeong09@users.noreply.github.com>
2025-11-28 15:18:19 +0900

    Title: 합과 평균, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251128/합과 평균/sum-and-avg.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+
+    cin >> a >> b;
+
+    double average = ((double) a + b) / 2;
+
+    cout << a + b << " " << average;
+
+    return 0;
+}
