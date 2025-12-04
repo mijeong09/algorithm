d80480841c78d9d125f918cdf709426cddf3d727
MEMEME <134994706+mijeong09@users.noreply.github.com>
2025-12-04 11:06:54 +0900

    Title: 배열의 합 계산하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251204/배열의 합 계산하기/calculate-sum-of-array.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int array[5] = {3, 1, 4, 5, 9};
+    cout << array[1] + array[3] + array[4];
+    
+    return 0;
+}
