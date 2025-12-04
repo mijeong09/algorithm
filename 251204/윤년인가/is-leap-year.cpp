940e8281b461cfa10c0c35fc837588deac8f1018
MEMEME <134994706+mijeong09@users.noreply.github.com>
2025-12-04 10:27:19 +0900

    Title: 윤년인가, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251204/윤년인가/is-leap-year.cpp
+++ 251204/윤년인가/is-leap-year.cpp
@@ -6,7 +6,7 @@ int main() {
     cin >> y;
 
     if(y % 4 == 0) {
-        if(y % 100 == 0 && y % 400 == 0) cout << "false";
+        if(y % 100 == 0 && y % 400 != 0) cout << "false";
         else cout << "true";
 
     } else cout << "false";
