911437a3e69566e6135001b0f6afe58a857086bf
MEMEME <134994706+mijeong09@users.noreply.github.com>
2025-12-05 14:39:18 +0900

    Title: 1부터의 합, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251205/1부터의 합/sum-from-the-one.cpp
+++ 251205/1부터의 합/sum-from-the-one.cpp
@@ -9,8 +9,10 @@ int main() {
     for(int i = 1; i <= 100; i++) {
         sum += i;
 
-        if(sum >= n) break;
-        cout << i;
+        if(sum >= n) {
+            cout << i;
+            break;
+        }
     }
 
     return 0;
