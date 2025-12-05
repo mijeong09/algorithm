bc134ce5353c89d867039ccc25d3636e23a41952
MEMEME <134994706+mijeong09@users.noreply.github.com>
2025-12-05 15:48:10 +0900

    Title: 직각 삼각형 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251205/직각 삼각형 출력/Right-triangle-and-output.cpp
+++ 251205/직각 삼각형 출력/Right-triangle-and-output.cpp
@@ -5,8 +5,8 @@ int main() {
     int n;
     cin >> n;
 
-    for (int i = 1; i <= n; i++) {
-        for (int j = 0; j < i * 2 - 1; j++) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i * 2 + 1; j++) {
             cout << "*";
         }
         cout << "\n";
