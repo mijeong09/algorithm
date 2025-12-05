fc1d23214143efba09fae404c3546c59c0136cba
MEMEME <134994706+mijeong09@users.noreply.github.com>
2025-12-05 14:37:17 +0900

    Title: 1부터의 합, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251205/1부터의 합/sum-from-the-one.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int sum = 0;
+    for(int i = 1; i <= 100; i++) {
+        sum += i;
+
+        if(sum >= n) break;
+    }
+
+    cout << sum;
+    
+    return 0;
+}
