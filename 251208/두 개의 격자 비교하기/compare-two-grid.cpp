28cad0b7a57dca1414fe43abf01b4482dfdea265
MEMEME <134994706+mijeong09@users.noreply.github.com>
2025-12-08 15:35:33 +0900

    Title: 두 개의 격자 비교하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251208/두 개의 격자 비교하기/compare-two-grid.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m, arr1[100][100], arr2[100][100], arr3[100][100];
+    cin >> n >> m;
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cin >> arr1[i][j];
+        }
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cin >> arr2[i][j];
+        }
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            if(arr1[i][j] == arr2[i][j]) {
+                arr3[i][j] = 0;
+            } else {
+                arr3[i][j] = 1;
+            }
+        }
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cout << arr3[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
