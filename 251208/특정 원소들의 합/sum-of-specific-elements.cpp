fb2cb4fe509cf002ea38e018bcde98dca27cc004
MEMEME <134994706+mijeong09@users.noreply.github.com>
2025-12-08 15:52:07 +0900

    Title: 특정 원소들의 합, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251208/특정 원소들의 합/sum-of-specific-elements.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[4][4], total = 0;
+
+    for(int i = 0; i < 4; i++) {
+        for(int j = 0; j < 4; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    for(int i = 0; i < 4; i++) {
+        for(int j = 0; j <= i; j++) {
+            total += arr[i][j];
+        }
+    }
+
+    cout << total;
+
+    return 0;
+}
