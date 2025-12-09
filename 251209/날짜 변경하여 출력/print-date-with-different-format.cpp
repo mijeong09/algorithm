dca16c053c8d56051a45d788ccb7a19d24d67c17
MEMEME <134994706+mijeong09@users.noreply.github.com>
2025-12-09 16:07:06 +0900

    Title: 날짜 변경하여 출력, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251209/날짜 변경하여 출력/print-date-with-different-format.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int y, m, d;
+    char c;
+
+    cin >> y >> c >> m >> d;
+
+    cout << m << "-" << d << "-" << y;
+    
+    return 0;
+}
