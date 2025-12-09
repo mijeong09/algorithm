a21d91af164e1dd41a5bd25bc28bd5b1b537434b
MEMEME <134994706+mijeong09@users.noreply.github.com>
2025-12-09 15:50:52 +0900

    Title: 세 실수의 반올림, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251209/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
+++ 251209/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int main() {
     double a, b, c;
-    int >> a >> b >> c;
+    cin >> a >> b >> c;
 
     cout << fixed;
     cout.precision(3);
     cout << a << "\n" << b << "\n" << c;
-    
+
     return 0;
 }
