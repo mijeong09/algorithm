093a37ced78b300073117aa51463c6edfb4ab37d
MEMEME <134994706+mijeong09@users.noreply.github.com>
2025-12-09 16:09:04 +0900

    Title: 전화번호 바꾸기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251209/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cin.get();
+    cin.get();
+    cin >> a;
+    cin.get();
+    cin >> b;
+
+    cout << 010 << "-" << a << "-" << b;
+    
+    return 0;
+}
