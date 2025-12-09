177fce8ab0294ad2c6b8087a94e345d75d87c1db
MEMEME <134994706+mijeong09@users.noreply.github.com>
2025-12-09 10:29:43 +0900

    Title: 좌우로 반복하며 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251209/좌우로 반복하며 출력/print-in-left-right-reverse.cpp
+++ 251209/좌우로 반복하며 출력/print-in-left-right-reverse.cpp
@@ -5,21 +5,15 @@ int main() {
     int n;
     cin >> n;
 
-    int arr[n];
-
-    for(int i = 0; i < n; i++) {
-        arr[i] = i + 1;
-    }
-
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
             if(i % 2 == 0) {
-                cout << arr[j];
-            } else {
-                cout << arr[n - j - 1];
-            }
+                cout << j + 1;
+            } else cout << n - j;
         }
         cout << endl;
     }
+
+
     return 0;
 }
