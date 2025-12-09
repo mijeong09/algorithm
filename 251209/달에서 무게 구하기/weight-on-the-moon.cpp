cdb96c038136df544b7925a49991e8c6f3c2333a
MEMEME <134994706+mijeong09@users.noreply.github.com>
2025-12-09 13:27:32 +0900

    Title: 달에서 무게 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251209/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int weight = 13;
+    double gravity = 0.165;
+
+    cout << fixed;
+
+    cout.precision(6);
+    cout << weight << " * " << gravity << " = " << weight * gravity;
+
+    return 0;
+}
