fc4a361f2ac4c631e44d172d9d3735c6d9a5e306
MEMEME <134994706+mijeong09@users.noreply.github.com>
2025-12-10 09:27:07 +0900

    Title: 길이 변형 후 사각형 넓이 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251210/길이 변형 후 사각형 넓이 구하기/square-width-after-length-change.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+
+    x += 8;
+    y *= 3;
+
+    cout << x << endl << y << endl << x * y;
+
+    return 0;
+}
