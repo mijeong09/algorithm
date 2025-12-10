25839c756039b064467f7804c833e4778331dfa6
MEMEME <134994706+mijeong09@users.noreply.github.com>
2025-12-10 10:00:59 +0900

    Title: 합과 평균의 차, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251210/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    
+    cin >> a >> b >> c;
+
+    int total = a + b + c;
+    int avg = total / 3;
+
+    cout << total << "\n" << avg <<  "\n" << total - avg;
+    
+    return 0;
+}
