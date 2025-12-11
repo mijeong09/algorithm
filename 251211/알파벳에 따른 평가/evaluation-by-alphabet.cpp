acdb37953caf83812b1e93270c74115daca47aab
MEMEME <134994706+mijeong09@users.noreply.github.com>
2025-12-11 10:00:50 +0900

    Title: 알파벳에 따른 평가, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251211/알파벳에 따른 평가/evaluation-by-alphabet.cpp
+++ 251211/알파벳에 따른 평가/evaluation-by-alphabet.cpp
@@ -10,8 +10,8 @@ int main() {
     else if(c == 'A') cout << "Excellent";
     else if(c == 'B') cout << "Good";
     else if(c == 'C') cout << "Usually";
-    else if(c == 'D') cout << "Effor";
+    else if(c == 'D') cout << "Effort";
     else cout << "Failure";
-    
+
     return 0;
 }
