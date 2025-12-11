d4a7caac40ca0d8a3183ca8f51b0f9da388be5c0
MEMEME <134994706+mijeong09@users.noreply.github.com>
2025-12-11 18:13:51 +0900

    Title: 별 N개 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251211/별 N개 출력하기/print-n-stars.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, i = 1;
+
+    cin >> n;
+
+    while(i <= n) {
+        cout << '*' << "\n";
+        i++;
+    }
+    
+    return 0;
+}
