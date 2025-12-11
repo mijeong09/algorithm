4e659a422bd45cb928f70759e27fd91827750d43
MEMEME <134994706+mijeong09@users.noreply.github.com>
2025-12-11 17:12:24 +0900

    Title: 일 수 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251211/일 수 구하기/number-of-days-in-month.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cin >> n;
+
+    if (n == 2) cout << 28;
+    else {
+        if((n < 8 && n % 2 == 1) || (n >= 8 && n % 2 == 0)) {
+            cout << 31;
+        } else cout << 30;
+    }
+    return 0;
+}
