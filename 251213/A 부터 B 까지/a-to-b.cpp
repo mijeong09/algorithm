beb775adc90aaa2d37c7ff16dac94ab626fa0026
MEMEME <134994706+mijeong09@users.noreply.github.com>
2025-12-13 13:13:58 +0900

    Title: A 부터 B 까지, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251213/A 부터 B 까지/a-to-b.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cin >> a >> b;
+
+    while(a <= b) {
+
+        cout << a << " ";
+
+        if(a % 2 != 0) {
+            a *= 2;
+        } else a += 3;
+    }
+
+    return 0;
+}
