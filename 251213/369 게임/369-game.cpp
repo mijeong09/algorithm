e55abb000854e937485eb414fa6eae761a6546ca
MEMEME <134994706+mijeong09@users.noreply.github.com>
2025-12-13 16:27:53 +0900

    Title: 369 게임, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251213/369 게임/369-game.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, cnt = 1;
+
+    cin >> n;
+
+    for(int i = 1; i <= n; i++) {
+
+        if(i % 3 == 0) cout << 0 << " ";
+        else if(cnt == 3) {
+            cout << 0 << " ";
+        } else {
+            cout << i << " ";
+        }
+        cnt++;
+
+        if(i % 10 == 0) cnt = 1;
+    }
+    return 0;
+}
