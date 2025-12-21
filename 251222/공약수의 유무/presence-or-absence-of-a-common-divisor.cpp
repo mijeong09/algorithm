3f431657a50d0937e23ee52edb1276fdc4100e55
MEMEME <134994706+mijeong09@users.noreply.github.com>
2025-12-22 08:48:04 +0900

    Title: 공약수의 유무, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251222/공약수의 유무/presence-or-absence-of-a-common-divisor.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    bool satisfied = false;
+
+    cin >> a >> b;
+
+    for(int i = a; i <= b; i++) {
+        if(1920 % i == 0 && 2880 % i == 0) {
+            satisfied = true;
+            break;
+        }
+    }
+
+    cout << satisfied;
+    
+    return 0;
+}
