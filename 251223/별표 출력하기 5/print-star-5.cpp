ce005db2725c36234ec3ca42a69fbc8cd913a10f
MEMEME <134994706+mijeong09@users.noreply.github.com>
2025-12-23 08:55:55 +0900

    Title: 별표 출력하기 5, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251223/별표 출력하기 5/print-star-5.cpp
+++ 251223/별표 출력하기 5/print-star-5.cpp
@@ -6,10 +6,10 @@ int main() {
 
     cin >> n;
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n - i; j++) {
-            for(int k = 0; k < n - i; k++) {
-                cout << '*';
+    for(int i = n; i > 0; i--) {
+        for(int j = 0; j < i; j++) {
+            for(int k = 0; k < i; k++) {
+                cout << "*";
             }
             cout << " ";
         }
