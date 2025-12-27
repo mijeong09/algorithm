15e8cf1d25256c270b028287d418558562acee79
MEMEME <134994706+mijeong09@users.noreply.github.com>
2025-12-27 16:24:43 +0900

    Title: 별표 출력하기 4, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 251227/별표 출력하기 4/print-star-4.cpp
+++ 251227/별표 출력하기 4/print-star-4.cpp
@@ -5,15 +5,15 @@ int main() {
     int n;
     cin >> n;
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n - i; j++) {
+    for(int i = n; i > 0; i--) {
+        for(int j = 0; j < i; j++) {
             cout << "* ";
         }
         cout << endl;
     }
 
-    for(int i = n - 2; i >= 0; i--) {
-        for(int j = 0; j < n - i; j++) {
+    for(int i = 1; i < n; i++) {
+        for(int j = 0; j < i; j++) {
             cout << "* ";
         }
         cout << endl;
