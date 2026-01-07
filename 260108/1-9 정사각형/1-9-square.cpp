42cfada38345582547054bc00855510e83aa1279
MEMEME <134994706+mijeong09@users.noreply.github.com>
2026-01-08 08:35:33 +0900

    Title: 1-9 정사각형, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260108/1-9 정사각형/1-9-square.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int cnt = 1;
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(cnt == 10) cnt = 1;
+
+            cout << cnt;
+            cnt++;
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
