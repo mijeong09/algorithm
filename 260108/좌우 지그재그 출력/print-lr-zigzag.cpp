9a4794492adc13278a58d571eb994f5c051a97c4
MEMEME <134994706+mijeong09@users.noreply.github.com>
2026-01-08 13:22:15 +0900

    Title: 좌우 지그재그 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260108/좌우 지그재그 출력/print-lr-zigzag.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int cnt = 1;
+    for(int i = 0; i < n; i++) {
+        if(i % 2 == 0) {
+            for(int j = 0; j < n; j++) {
+                cout << cnt << " ";
+                cnt++;
+            }
+        }
+        else {
+            cnt += n -1;
+            for(int j = 0; j < n; j++) {
+                cout << cnt - j << " ";
+            }
+            cnt++;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
