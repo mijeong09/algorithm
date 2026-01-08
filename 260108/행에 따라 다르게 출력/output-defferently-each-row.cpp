df77821f67700e7d7b1253cb35dd642c0075a02d
MEMEME <134994706+mijeong09@users.noreply.github.com>
2026-01-08 13:31:02 +0900

    Title: 행에 따라 다르게 출력, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260108/행에 따라 다르게 출력/output-defferently-each-row.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int cnt = 0;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(i % 2 == 0) {
+                cnt++;
+                cout << cnt << " ";
+            }
+            else {
+                cnt += 2;
+                cout << cnt << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
