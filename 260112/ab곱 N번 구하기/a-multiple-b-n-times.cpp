898b93d3216e19dc4a997165400ae03f8dc165ca
MEMEME <134994706+mijeong09@users.noreply.github.com>
2026-01-12 08:37:40 +0900

    Title: ab곱 N번 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260112/ab곱 N번 구하기/a-multiple-b-n-times.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+
+        int mul = 1;
+
+        for(int i = a; i <= b; i++) {
+            mul *= i;
+        }
+        
+        cout << mul << endl;
+    }
+    
+    return 0;
+}
