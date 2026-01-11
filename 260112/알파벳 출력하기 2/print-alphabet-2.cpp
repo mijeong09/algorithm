df585dd842e93ef9ec97b43bf222c83cf9110e8b
MEMEME <134994706+mijeong09@users.noreply.github.com>
2026-01-12 08:32:38 +0900

    Title: 알파벳 출력하기 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260112/알파벳 출력하기 2/print-alphabet-2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    char c = 'A';
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < i; j++) {
+            cout << "  ";
+        }
+        for(int j = 0; j < n - i; j++) {
+            cout << c << " ";
+            c++;
+
+            if(c > 'Z') c = 'A';
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
