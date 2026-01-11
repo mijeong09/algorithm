ad3091604cb5d8dff995cfa71ca78091d583d316
MEMEME <134994706+mijeong09@users.noreply.github.com>
2026-01-11 16:36:09 +0900

    Title: 19단 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260111/19단 출력/nineteen-times-table.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    for(int i = 1; i <= 19; i++) {
+        for(int j = 1; j <= 19; j++) {
+            if(j % 2 == 0 || j == 19) {
+                cout << i << " * " << j << " = " << i * j << endl;
+            }
+            else cout << i << " * " << j << " = " << i * j << " / ";
+        }
+    }
+    return 0;
+}
