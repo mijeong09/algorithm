94a64ec466fbf06fb42a8027002a566cfbf3e00f
MEMEME <134994706+mijeong09@users.noreply.github.com>
2026-01-12 13:30:46 +0900

    Title: 학점 계산기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260112/학점 계산기/credit-calculator.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    double arr[5];
+    double sum_val = 0;
+
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+        sum_val += arr[i];
+    }
+
+    double avg = (double)sum_val / n;
+
+    cout << fixed;
+    cout.precision(1);
+
+    cout << avg << endl;
+
+    if(avg >= 4.0) cout << "Perfect";
+    else if(avg >= 3.0) cout << "Good";
+    else cout << "Poor";
+
+    return 0;
+}
