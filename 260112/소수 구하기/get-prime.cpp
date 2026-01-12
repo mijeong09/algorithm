a07f7480311f8e86e07406a0ff2314939629e871
MEMEME <134994706+mijeong09@users.noreply.github.com>
2026-01-12 11:03:06 +0900

    Title: 소수 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260112/소수 구하기/get-prime.cpp
+++ 260112/소수 구하기/get-prime.cpp
@@ -7,14 +7,15 @@ int main() {
 
     for(int i = 1; i <= n; i++) {
         
-        int cnt = 0;
+        if(i == 1) continue;
+        bool isPrime = true;
 
-        for(int j = 1; j <= n; j++) {
-            if(i % j == 0) cnt++;
+        for(int j = 2; j < i; j++) {
+            if(i % j == 0) isPrime = false;
         }
         
-        if(cnt == 2) cout << i << " ";
+        if(isPrime) cout << i << " ";
     }
-    
+
     return 0;
 }
