c64c5e0cdda1fa0a59ee51697c2a171e3b152e8e
MEMEME <134994706+mijeong09@users.noreply.github.com>
2026-01-13 16:47:21 +0900

    Title: 개수 세기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260113/개수 세기/count-numbers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[100];
+    int cnt = 0;
+    int n, m;
+
+    cin >> n >> m;
+
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+
+        if(arr[i] == m) cnt++;
+    }
+
+    cout << cnt;
+    
+    return 0;
+}
