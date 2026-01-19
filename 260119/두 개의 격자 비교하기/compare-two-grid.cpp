7e20acebddff19784b48cdd89dad619f022a478e
MEMEME <134994706+mijeong09@users.noreply.github.com>
2026-01-19 10:51:14 +0900

    Title: 두 개의 격자 비교하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260119/두 개의 격자 비교하기/compare-two-grid.cpp
+++ 260119/두 개의 격자 비교하기/compare-two-grid.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     int n, m;
-    int arr1[10][10], arr2[10][10];
+    int arr1[10][10], arr2[10][10], arr3[10][10];
 
     cin >> n >> m;
 
@@ -21,8 +21,13 @@ int main() {
 
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
-            if (arr1[i][j] == arr2[i][j]) cout << 0 << " ";
-            else cout << 1 << " ";
+            arr3[i][j] = arr1[i][j] == arr2[i][j] ? 0 : 1;
+        }
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cout << arr3[i][j] << " ";
         }
         cout << endl;
     }
