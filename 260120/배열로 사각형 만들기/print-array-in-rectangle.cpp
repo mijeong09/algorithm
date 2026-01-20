ad961fe41435837ea150c479c0544b68a67f4098
MEMEME <134994706+mijeong09@users.noreply.github.com>
2026-01-20 15:54:13 +0900

    Title: 배열로 사각형 만들기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260120/배열로 사각형 만들기/print-array-in-rectangle.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5][5] = {0, };
+
+    for(int i = 0; i < 5; i++) {
+        arr[0][i] = 1;
+        arr[i][0] = 1;
+    }
+
+    for(int i = 1; i < 5; i++) {
+        for(int j = 1; j < 5; j++) {
+            arr[i][j] = arr[i - 1][j] + arr[i][j - 1];
+        }
+    }
+
+    for(int i = 0; i < 5; i++) {
+        for(int j = 0; j < 5; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
