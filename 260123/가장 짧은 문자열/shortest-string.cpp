52340d34cd24034613295135cc7318fed1140d2c
MEMEME <134994706+mijeong09@users.noreply.github.com>
2026-01-23 10:50:51 +0900

    Title: 가장 짧은 문자열, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260123/가장 짧은 문자열/shortest-string.cpp
+++ 260123/가장 짧은 문자열/shortest-string.cpp
@@ -7,11 +7,9 @@ int main() {
 
     cin >> str1 >> str2 >> str3;
 
-    int len1, len2, len3;
-
-    len1 = str1.length();
-    len2 = str2.length();
-    len3 = str3.length();
+    int len1 = str1.length();
+    int len2 = str2.length();
+    int len3 = str3.length();
 
     if(len1 >= len2 && len1 >= len3) {
         if(len2 >= len3) cout << len1 - len3;
