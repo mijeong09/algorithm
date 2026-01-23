c9585b19067f1835f0ed8c3a5f41bdf787436c4b
MEMEME <134994706+mijeong09@users.noreply.github.com>
2026-01-23 10:58:06 +0900

    Title: 문자 개수 세기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260123/문자 개수 세기/count-char.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+    char c;
+
+    getline(cin, str);
+    cin >> c;
+
+    int cnt = 0;
+    for(int i = 0; i < str.length(); i++) {
+        if(str[i] == c) cnt++;
+    }
+
+    cout << cnt;
+    
+    return 0;
+}
