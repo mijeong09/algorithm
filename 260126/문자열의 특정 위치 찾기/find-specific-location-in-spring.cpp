71d6bcf5f17acf9066a6f75e7103ade66af2f0d5
MEMEME <134994706+mijeong09@users.noreply.github.com>
2026-01-26 16:42:33 +0900

    Title: 문자열의 특정 위치 찾기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260126/문자열의 특정 위치 찾기/find-specific-location-in-spring.cpp
+++ 260126/문자열의 특정 위치 찾기/find-specific-location-in-spring.cpp
@@ -5,10 +5,18 @@ using namespace std;
 int main() {
     string str;
     char a;
+    int idx = -1;
 
     cin >> str >> a;
 
-    if(str.find(a) != string::npos) cout << str.find(a);
+    for(int i = 0; str[i] != '\0'; i++) {
+        if(str[i] == a) {
+            idx = i;
+            break;
+        }
+    }
+
+    if(idx != -1) cout << idx;
     else cout << "No";
 
     return 0;
