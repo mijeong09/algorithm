9c077c49800156441277bfa41c5560c8105a97e3
MEMEME <134994706+mijeong09@users.noreply.github.com>
2026-01-26 15:12:32 +0900

    Title: 두 문자열을 이어붙였을 때, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260126/두 문자열을 이어붙였을 때/when-two-strings-are-concatenated.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string a, b;
+
+    cin >> a >> b;
+
+    if((a + b) == (b + a)) {
+        cout << "true";
+    } else {
+        cout << "false";
+    }
+
+    return 0;
+}
