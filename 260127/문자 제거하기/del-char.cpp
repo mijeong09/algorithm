87ec904d75be463617c28e4169d98b67e92e20eb
MEMEME <134994706+mijeong09@users.noreply.github.com>
2026-01-27 10:45:56 +0900

    Title: 문자 제거하기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260127/문자 제거하기/del-char.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string str;
+    int n;
+
+    cin >> str;
+
+    while(true) {
+        cin >> n;
+
+        if(n > str.length()) {
+            str.erase(str.length() - 1, 1);
+        }
+        else {
+            str.erase(n, 1);
+        }
+
+        cout << str << endl;
+
+        if(str.length() == 1) break;
+    }
+
+    return 0;
+}
