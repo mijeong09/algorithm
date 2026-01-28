a4e7b57f9410b14c4048af0662414773815918d3
MEMEME <134994706+mijeong09@users.noreply.github.com>
2026-01-28 11:40:59 +0900

    Title: 그 전 알파벳, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260128/그 전 알파벳/before-the-alphabet.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    char a;
+
+    cin >> a;
+
+    if(a == 'a') cout << 'z';
+    else cout << (char)(a - 1);
+    
+    return 0;
+}
