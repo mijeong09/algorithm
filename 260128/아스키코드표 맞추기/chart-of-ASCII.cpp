cee7c13e79919497bafbdc9a59f22b811af1de24
MEMEME <134994706+mijeong09@users.noreply.github.com>
2026-01-28 11:30:25 +0900

    Title: 아스키코드표 맞추기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260128/아스키코드표 맞추기/chart-of-ASCII.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    for(int i = 0; i < 5; i++) {
+        int n;
+        cin >> n;
+
+        cout << (char)n << " ";
+    }
+    
+    return 0;
+}
