dd66c831dcdd42092277a76ce799a8caa68ad6d0
MEMEME <134994706+mijeong09@users.noreply.github.com>
2026-01-28 10:57:49 +0900

    Title: 문자열 밀기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260128/문자열 밀기/push-char.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string str;
+
+    cin >> str;
+
+    int len = str.length();
+    str = str.substr(1, len - 1) + str.substr(0, 1);
+
+    cout << str;
+    
+    return 0;
+}
