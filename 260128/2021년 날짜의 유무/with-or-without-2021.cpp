177f5280fcd3762b8f1b9db988c9cf7632074b3e
MEMEME <134994706+mijeong09@users.noreply.github.com>
2026-01-28 20:18:21 +0900

    Title: 2021년 날짜의 유무, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260128/2021년 날짜의 유무/with-or-without-2021.cpp
+++ 260128/2021년 날짜의 유무/with-or-without-2021.cpp
@@ -7,7 +7,7 @@ int M, D;
 bool Func(int m, int d) {
     int month[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-    if(d <= month[m - 1]) return true;
+    if(m <= 12 && d <= month[m - 1]) return true;
     else return false;
 }
 
@@ -16,6 +16,6 @@ int main() {
 
     if(Func(M, D)) cout << "Yes";
     else cout << "No";
-    
+
     return 0;
 }
