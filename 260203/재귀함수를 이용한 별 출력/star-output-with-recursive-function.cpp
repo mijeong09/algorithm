fdb909b039bc95616e2885b9635d264dc282ccbf
MEMEME <134994706+mijeong09@users.noreply.github.com>
2026-02-03 08:46:01 +0900

    Title: 재귀함수를 이용한 별 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260203/재귀함수를 이용한 별 출력/star-output-with-recursive-function.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+void Print(int n) {
+    if(n == 0) return;
+
+    Print(n - 1);
+
+    for(int i = 0; i < n; i++) {
+        cout << '*';
+    }
+    cout << endl;
+}
+
+
+int main() {
+    int n;
+
+    cin >> n;
+
+    Print(n);
+
+    return 0;
+}
