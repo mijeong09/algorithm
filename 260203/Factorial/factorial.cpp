f3834d9f5d3d2c29b4ce28b8a2cf72828c899b40
MEMEME <134994706+mijeong09@users.noreply.github.com>
2026-02-03 13:33:22 +0900

    Title: Factorial, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260203/Factorial/factorial.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int Fact(int n) {
+    if(n == 0) return 1;
+
+    return Fact(n - 1) * n;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    cout << Fact(n);
+    
+    return 0;
+}
